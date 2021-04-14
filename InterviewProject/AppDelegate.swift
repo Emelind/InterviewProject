aa2f61c3f73f5134136f4178adbf3095a46275c9
emelie <emeliejlindvall@gmail.com>
2021-04-14 14:30:18 +0200

    Initial commit

--- InterviewProject/AppDelegate.swift
+++ InterviewProject/AppDelegate.swift
@@ -9,13 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+            window = UIWindow()
+            window?.rootViewController = ViewController()
+            window?.makeKeyAndVisible()
+        
+            return true
+        }
 
     // MARK: UISceneSession Lifecycle
 
